4b431b4937f12e68f2e3a530a65d7844b2139c9c
Zainab Murtaza <162459926+Zainab090@users.noreply.github.com>
2024-05-20 22:02:52 +0500

    Javascript assignment#3

---
+++ app.js
@@ -0,0 +1,16 @@
+//This is 1st assignment
+let age=15;
+alert("I'm "+age +" years old")
+//This is 2nd assignment
+var visits =0;
+visits ++;
+alert("You have visited this site "+visits+" times");
+//This is 3rd assignment
+let birthYear=2007;
+alert("My birth year is "+birthYear+".\n Datatype of my declare varible is Number")
+//This is 4th assignment
+let visitorName="John Doe";
+let productTitle="T-shirt(s)";
+let quantity="5";
+document.write("<b>" + visitorName + "</b> ordered <b>" + quantity + " " + productTitle + "</b> on XYZ Clothing store");  
+//Assignment completed :)
